d6367114caac2569969badd246774a9e6be5a8c4
BattleDemon <143239271+BattleDemon@users.noreply.github.com>
2025-06-03 17:21:33 +1000

    egwg

--- Enemies/ork_scout.gd
+++ Enemies/ork_scout.gd
@@ -14,7 +14,6 @@ extends CharacterBody2D
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
 @export var attack_range: float = 20.0
 @export var attack_damage: int = 10
 @export var attack_cooldown: float = 2.5
@@ -30,8 +29,6 @@ var is_returning_to_patrol := false
 <<<<<<< Updated upstream
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 =======
-=======
->>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
