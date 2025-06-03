5fdd54fe34faf0eb169a541331b3d442c2d7c1c6
Dexter Hart <143239271+BattleDemon@users.noreply.github.com>
2025-06-03 13:32:53 +1000

    igtonwegwejng

--- Enemies/ork_scout.gd
+++ Enemies/ork_scout.gd
@@ -12,6 +12,7 @@ extends CharacterBody2D
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 @export var attack_range: float = 20.0
 @export var attack_damage: int = 10
 @export var attack_cooldown: float = 2.5
@@ -27,6 +28,8 @@ var is_returning_to_patrol := false
 <<<<<<< Updated upstream
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
