01d59f28fa80668b33f9ed98e2df98dbfa74c683
BattleDemon <143239271+BattleDemon@users.noreply.github.com>
2025-06-19 21:42:06 +1000

    removed my documentation images and the start screen

--- Scenes/title_screen.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	$AudioStreamPlayer.play(5) #Plays music at 5 seconds in
-	
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("play"): #Changes scene when space is pressed
-		get_tree().change_scene_to_file("res://Scenes/ashefootglade/ashefootglade.tscn") 
