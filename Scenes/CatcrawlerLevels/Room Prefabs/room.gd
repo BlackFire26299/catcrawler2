daa9c82f9c4e6bda0016712e8812d146f40b8f14
Dexter Hart <143239271+BattleDemon@users.noreply.github.com>
2025-07-14 15:14:47 +1000

    can spawn objects in room

--- Scenes/CatcrawlerLevels/Room Prefabs/room.gd
+++ Scenes/CatcrawlerLevels/Room Prefabs/room.gd
@@ -2,11 +2,31 @@ class_name Room
 extends Node2D 
 
 @export var type: GlobalEnums.RoomType 
-@export var minNumDirt: int
-@export var maxNumDirt: int
+@export var minNumDirt: int = 1
+@export var maxNumDirt: int = 6
 
 @export var minEnemies: int
 @export var maxEnemies: int
 
+@onready var spawnPointparent = $ObjectSpawns
+
+var dirtpile = preload("res://Objects/trash_pile.tscn")
+
+var rng = RandomNumberGenerator.new()
+
+var seenRoom = false
+
 func spawnRoomObjects():
-	pass
+	var spawnpoints = spawnPointparent.get_children()
+	var rngNumOfObjects = randi_range(minNumDirt,maxNumDirt)
+	for object in rngNumOfObjects:
+		print("this code ran")
+		var spawnindex = randi() % spawnpoints.size()
+		var rngChild = spawnpoints[spawnindex]
+		var pile = dirtpile.instantiate()
+		rngChild.add_child(pile)
+
+func spawnRoomEnemies():
+	if !seenRoom:
+		seenRoom = true
+		pass
